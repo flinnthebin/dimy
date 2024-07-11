a0b95377fb58c1738622dd755813cdb5d7585d73
Chris Flinn <christopher.flinn@gmail.com>
2024-07-12 01:43:19 +1000

    UDP broadcast

--- ephemeral_id.py
+++ ephemeral_id.py
@@ -1,26 +1,23 @@
 import time
 import uuid
 import shamirs
+import socket
 
-# https://docs.python.org/3/library/time.html#time.sleep
-# https://docs.python.org/3/library/uuid.html#uuid.uuid4
-# https://docs.python.org/3/library/random.html#module-random
-
-# Task 1: Generate a 32-Byte Ephemeral ID (EphID) after every 15 sec.
-
+# CONFIG 
+udp_broadcast_ip = '127.0.0.1'
+udp_broadcast_port = 8080 
 mersenne_prime = (2**607) - 1
-
+# UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 def int_encode(s):
     return int.from_bytes(s.encode(), "big")
 
-
 def string_encode(i):
     return i.to_bytes((i.bit_length() + 7) // 8, "big").decode()
 
-
 while True:
-    time.sleep(1.5)
     alpha_key, bravo_key = str(uuid.uuid4()), str(uuid.uuid4())
     ephemeral_id = alpha_key + "-" + bravo_key
     shares = shamirs.shares(
@@ -28,14 +25,8 @@ while True:
     )
 
     for share in shares:
-        print(share)
+        sock.sendto(str(share).encode(), (udp_broadcast_ip, udp_broadcast_port))
+        time.sleep(3)
 
     reconstructed_int = shamirs.interpolate(shares[:3])
     reconstructed_ephemeral_id = string_encode(reconstructed_int)
-
-    print(f"Original EphID: {ephemeral_id}")
-    print(f"Reconstructed EphID: {reconstructed_ephemeral_id}")
-    print("Shares:")
-    for share in shares:
-        print(share)
-    print("\n")
