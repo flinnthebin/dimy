0aa721949a4c054bc4dce61511c967360cf02611
Chris Flinn <christopher.flinn@gmail.com>
2024-07-12 01:59:05 +1000

    refactor

---
+++ dimy.py
@@ -0,0 +1,50 @@
+import time
+import uuid
+import shamirs
+import socket
+
+class Node:
+    def __init__(self, udp_broadcast_ip='127.0.0.1', udp_broadcast_port=55000, mersenne_prime=(2**607) - 1):
+        self.udp_broadcast_ip = udp_broadcast_ip
+        self.udp_broadcast_port = udp_broadcast_port
+        self.mersenne_prime = mersenne_prime
+        self.ephid_counter = 1
+
+        # UDP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    def int_encode(self, s):
+        return int.from_bytes(s.encode(), "big")
+
+    def string_encode(self, i):
+        return i.to_bytes((i.bit_length() + 7) // 8, "big").decode()
+
+    def generate_ephemeral_id(self):
+        alpha_key, bravo_key = str(uuid.uuid4()), str(uuid.uuid4())
+        return alpha_key + "-" + bravo_key
+
+    def share_ephemeral_id(self, ephemeral_id):
+        return shamirs.shares(
+            self.int_encode(ephemeral_id), 5, modulus=self.mersenne_prime, threshold=3
+        )
+
+    def broadcast_shares(self):
+        while True:
+            ephemeral_id = self.generate_ephemeral_id()
+            shares = self.share_ephemeral_id(ephemeral_id)
+
+            for i, share in enumerate(shares, start=1):
+                message = f"EphID #{self.ephid_counter}: {share}"
+                self.sock.sendto(message.encode(), (self.udp_broadcast_ip, self.udp_broadcast_port))
+                time.sleep(3)
+
+            self.ephid_counter += 1
+
+if __name__ == "__main__":
+    node = Node()
+    node.broadcast_shares()
+
+################ REQUIRED LATER ####################################
+#   reconstructed_int = shamirs.interpolate(shares[:3])
+#   reconstructed_ephemeral_id = string_encode(reconstructed_int)
